b5e2b7d88bd8261db2fba207086a8d9563b01614
Dahlia Ghonim <dahlia.ghonim@gmail.com>
2026-04-03 03:14:59 +0200

    Update favicon and page title

--- src/router/index.js
+++ src/router/index.js
@@ -13,33 +13,55 @@ const router = new VueRouter({
       name: "home",
       path: "/",
       component: HomePage,
+      meta: {
+        title: "Home | Exclusive",
+      },
     },
     {
       name: "about",
       path: "/about",
       component: () => import("@/views/AboutUs.vue"),
+      meta: {
+        title: "About | Exclusive",
+      },
     },
     {
       name: "contact",
       path: "/contact",
       component: () => import("@/views/ContactUs.vue"),
+      meta: {
+        title: "Contact | Exclusive",
+      },
     },
     {
       name: "products",
       path: "/products",
       component: () => import("@/views/ProductsPage.vue"),
+      meta: {
+        title: "Products | Exclusive",
+      },
     },
     {
       name: "product_details",
       path: "/product/:id",
       component: () => import("@/views/ProductDetails.vue"),
+      meta: {
+        title: "Product Details | Exclusive",
+      },
     },
     {
       name: "not_found",
       path: "*",
       component: () => import("@/views/NotFound.vue"),
+      meta: {
+        title: "Not Found | Exclusive",
+      },
     },
   ],
+});
+
+router.afterEach((to) => {
+  document.title = to.meta.title || "Exclusive";
 });
 
 export default router;
