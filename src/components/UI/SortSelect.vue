efcdaacef318d6720cd633e473cdfcdc048072ba
Dahlia Ghonim <dahlia.ghonim@gmail.com>
2026-04-10 08:11:14 +0200

    Add products page and styling (with static products) Also added SortSelect sub-component

---
+++ src/components/UI/SortSelect.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <label class="heading-20px-regular" for="sort-select__input">Sort by</label>
+    <div class="title-14px-regular sort-select__wrapper">
+      <img class="sort-select__arrow" :src="downIcon" />
+      <select class="sort-select__input" id="sort-select__input">
+        <option>Highest Rating</option>
+        <option>Price: Low to high</option>
+        <option>Price: High to low</option>
+        <option>Discount Percentage</option>
+        <option>Brand</option>
+        <option>Category</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+import downIcon from "@/assets/images/components/shared/icon-down.png";
+
+export default {
+  name: "SortSelect",
+  data() {
+    return {
+      downIcon,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.sort-select__wrapper {
+  position: relative;
+  display: inline-block;
+  margin-left: 12px; /* TODO: Not specified by Figma design */
+}
+
+.sort-select__input {
+  /* Hide the default arrow */
+  appearance: none;
+  -webkit-appearance: none;
+
+  background-color: var(--color-dark-gray);
+  color: var(--color-white);
+  padding: 15px 20px;
+  border: 1px solid var(--color-black);
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.sort-select__arrow {
+  position: absolute;
+  right: 12.27px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  width: 12.73px;
+  height: 7.78px;
+  cursor: pointer;
+}
+</style>
--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,7 +1,67 @@
 <template>
-  <div>Products page</div>
+  <div class="products">
+    <header class="products__header">
+      <AppBreadcrumb current="Products" />
+      <div class="products__header-main">
+        <h2 class="heading">Explore Our Products</h2>
+        <SortSelect />
+      </div>
+    </header>
+    <div class="products__grid">
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+      <ProductCard :isNew="false" :hasDiscount="false" />
+    </div>
+    <AppButton class="products__load-btn">Load more ...</AppButton>
+  </div>
 </template>
 
-<script></script>
+<script>
+import AppBreadcrumb from "@/components/layout/AppBreadcrumb.vue";
+import SortSelect from "@/components/UI/SortSelect.vue";
+import ProductCard from "@/components/product/ProductCard.vue";
+import AppButton from "@/components/shared/AppButton.vue";
 
-<style scoped></style>
+export default {
+  name: "ProductsPage",
+  components: {
+    AppBreadcrumb,
+    SortSelect,
+    ProductCard,
+    AppButton,
+  },
+};
+</script>
+
+<style scoped>
+.products {
+  padding: 20px 40px; /* TODO: figma design deviation (53px 125px) */
+}
+
+.products__header-main {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.products__grid {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  column-gap: 30px;
+  row-gap: 68px;
+  margin-top: 60px;
+}
+
+.products__load-btn {
+  margin: 69px auto;
+  display: block;
+}
+</style>
